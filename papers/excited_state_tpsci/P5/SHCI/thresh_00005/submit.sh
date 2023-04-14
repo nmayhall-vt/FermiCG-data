bc3a27040bf9a53fc4888cc8f86ea23dcc88f63e
Nicole Braunscheidel <nbraunsc@vt.edu>
2023-04-14 17:05:44 -0400

    added SHCI data for P5

---
+++ papers/excited_state_tpsci/P5/SHCI/thresh_00005/submit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=128
+#SBATCH -t 60:00:00
+#SBATCH -p largemem_q
+#SBATCH -A nmayhall_group
+#SBATCH --job-name shci_00005
+
+module reset
+module load gcc/8.2.0
+module load openmpi/gcc/64/4.0.4
+export OMP_NUM_THREADS=1
+
+
+cd $SLURM_SUBMIT_DIR
+
+# run job
+mpirun -n 24 /home/vibin1/tinkercliff_shci/shci_excited/shci/shci
+
+exit;
