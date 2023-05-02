465d6403badf11c4426bbd1375a65891318a4681
Nick Mayhall <nmayhall@vt.edu>
2023-05-01 21:32:26 -0400

    working

--- bimetallics/cr2_morokuma/16__3d4s_2p2s_3d4s/bst_01.jl
+++ bimetallics/cr2_morokuma/16__3d4s_2p2s_3d4s/bst_01.jl
@@ -25,10 +25,10 @@ nroots=4
 p_spaces = Vector{ClusterSubspace}()
 
 ssi = ClusterSubspace(clusters[1])
-add_subspace!(ssi, (3,0), 1:1)
-add_subspace!(ssi, (2,1), 1:1)
-add_subspace!(ssi, (1,2), 1:1)
-add_subspace!(ssi, (0,3), 1:1)
+add_subspace!(ssi, (4,1), 1:1)
+add_subspace!(ssi, (3,2), 1:1)
+add_subspace!(ssi, (2,3), 1:1)
+add_subspace!(ssi, (1,4), 1:1)
 push!(p_spaces, ssi)
 
 ssi = ClusterSubspace(clusters[2])
@@ -36,33 +36,24 @@ add_subspace!(ssi, (4,4), 1:1)
 push!(p_spaces, ssi)
 
 ssi = ClusterSubspace(clusters[3])
-add_subspace!(ssi, (3,0), 1:1)
-add_subspace!(ssi, (2,1), 1:1)
-add_subspace!(ssi, (1,2), 1:1)
-add_subspace!(ssi, (0,3), 1:1)
+add_subspace!(ssi, (4,1), 1:1)
+add_subspace!(ssi, (3,2), 1:1)
+add_subspace!(ssi, (2,3), 1:1)
+add_subspace!(ssi, (1,4), 1:1)
 push!(p_spaces, ssi)
-
 
 
 ci_vector = BSTstate(clusters, p_spaces, cluster_bases, R=4) 
 
-na = 18 
-nb = 18
+na = 9 
+nb = 9
 FermiCG.fill_p_space!(ci_vector, na, nb)
-# FermiCG.eye!(ci_vector)
-# ebst, vbst = FermiCG.ci_solve(ci_vector, cluster_ops, clustered_ham)
+FermiCG.eye!(ci_vector)
+ebst, vbst = FermiCG.ci_solve(ci_vector, cluster_ops, clustered_ham)
+
+ebst, vbst = FermiCG.block_sparse_tucker(vbst, cluster_ops, clustered_ham, thresh_var=1e-2, thresh_foi=1e-6, thresh_pt=1e-4)
 
 
+e2a = FermiCG.compute_pt2_energy(vbst, cluster_ops, clustered_ham)
 
-ci_vector = FermiCG.TPSCIstate(clusters, init_fspace, R=nroots)
 
-ci_vector = FermiCG.add_spin_focksectors(ci_vector)
-
-eci, v = FermiCG.tps_ci_direct(ci_vector, cluster_ops, clustered_ham);
-
-e0a, v0a = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham, incremental=true,
-                            thresh_cipsi = 1e-4, 
-                            thresh_foi   = 1e-6,
-                            thresh_asci  = -1);
-
-e2a = FermiCG.compute_pt2_energy(v0a, cluster_ops, clustered_ham)
