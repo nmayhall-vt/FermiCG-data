43ebf12e4ef9e3c4b9cb6f6b89f4335d7fbbb3cc
Nick Mayhall <nmayhall@vt.edu>
2023-05-19 12:24:14 -0400

    added tpsci out

---
+++ bimetallics/fe2s2_sharma/28__3d_3p_3p_3d_3p_3p_3p_3p/m100/tpsci_01.jl
@@ -0,0 +1,61 @@
+using QCBase
+using FermiCG
+using NPZ
+using InCoreIntegrals
+using RDM
+using JLD2
+using LinearAlgebra
+
+@load "../data_cmf.jld2"
+
+M = 100
+
+BLAS.get_config()
+
+init_fspace =  FockConfig([(5, 0), (3, 3), (3, 3), (0, 5), (3, 3), (3, 3), (3, 3), (3, 3)])
+
+
+cluster_bases = FermiCG.compute_cluster_eigenbasis_spin(ints, clusters, d1, [3,3,3,3,3,3,3,3], init_fspace, max_roots=M, verbose=1);
+
+clustered_ham = FermiCG.extract_ClusteredTerms(ints, clusters);
+cluster_ops = FermiCG.compute_cluster_ops(cluster_bases, ints);
+
+FermiCG.add_cmf_operators!(cluster_ops, cluster_bases, ints, d1.a, d1.b);
+
+nroots=6
+
+
+ci_vector = FermiCG.TPSCIstate(clusters, init_fspace, R=nroots)
+
+ci_vector = FermiCG.add_spin_focksectors(ci_vector)
+
+eci, v = FermiCG.tps_ci_direct(ci_vector, cluster_ops, clustered_ham);
+
+values = [6.4e-4, 3.2e-4, 1.6e-4, 8e-5,  4e-5, 2e-5]
+
+
+for v in values
+    
+    e0a, v0a = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham,
+                                thresh_cipsi = v,
+                                thresh_spin  = v,
+                                thresh_foi   = 1e-6,
+                                ci_max_iter  = 100,
+                                conv_thresh  = 1e-5,
+                                nbody        = 4,
+                                max_mem_ci   = 50);
+
+    @save "out.jld2" ci_vector v0a e0a cluster_bases clustered_ham init_fspace v 
+
+    e2a = FermiCG.compute_pt2_energy(v0a, cluster_ops, clustered_ham)
+   
+
+    rotations = FermiCG.hosvd(v0a, cluster_ops)
+    for ci in clusters
+        FermiCG.rotate!(cluster_ops[ci.idx], rotations[ci.idx])
+        FermiCG.rotate!(cluster_bases[ci.idx], rotations[ci.idx])
+        FermiCG.check_basis_orthogonality(cluster_bases[ci.idx])
+    end
+
+end
+
