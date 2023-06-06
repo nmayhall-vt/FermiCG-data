afb8257f68058cc608717af43c16c7ceadaabd67
Nick Mayhall <nmayhall@vt.edu>
2023-06-06 15:46:52 -0400

    added plots

---
+++ bimetallics/fe2_morokuma/rohf.def2tzvp/26__3d4d_2p3p/plots.sh
@@ -0,0 +1,15 @@
+python plot_extrap.py m010/energies.csv > m010/energies_extrap.out
+python plot_extrap.py m020/energies.csv > m020/energies_extrap.out
+python plot_extrap.py m030/energies.csv > m030/energies_extrap.out
+python plot_extrap.py m040/energies.csv > m040/energies_extrap.out
+python plot_extrap.py m050/energies.csv > m050/energies_extrap.out
+python plot_extrap.py m100/energies.csv > m100/energies_extrap.out
+python plot_extrap.py m200/energies.csv > m200/energies_extrap.out
+
+python plot_gaps.py m010/energies.csv > m010/energies_gaps.out
+python plot_gaps.py m020/energies.csv > m020/energies_gaps.out
+python plot_gaps.py m030/energies.csv > m030/energies_gaps.out
+python plot_gaps.py m040/energies.csv > m040/energies_gaps.out
+python plot_gaps.py m050/energies.csv > m050/energies_gaps.out
+python plot_gaps.py m100/energies.csv > m100/energies_gaps.out
+python plot_gaps.py m200/energies.csv > m200/energies_gaps.out
