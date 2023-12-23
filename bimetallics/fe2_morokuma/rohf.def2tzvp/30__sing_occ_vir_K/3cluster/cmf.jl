236c66698c8772e02ec512381dc39426675afa44
Nick Mayhall <nmayhall@vt.edu>
2023-12-23 16:43:53 -0500

    added dmet

--- bimetallics/fe2_morokuma/rohf.def2tzvp/30__sing_occ_vir_K/3cluster/cmf.jl
+++ bimetallics/fe2_morokuma/rohf.def2tzvp/30__sing_occ_vir_K/3cluster/cmf.jl
@@ -1,5 +1,6 @@
 using QCBase
 using ClusterMeanField
+using ActiveSpaceSolvers 
 using NPZ
 using InCoreIntegrals
 using RDM
@@ -22,27 +23,27 @@ Pb = npzread("../Pb.npy")
 #clusters    = [[2, 3, 4], [5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [1, 15, 16, 17], [18, 19, 20, 21, 22, 23, 24, 25, 26, 27], [28, 29, 30]]
 #init_fspace = [(3, 3), (5, 0), (4, 4), (0, 5), (3, 3)]
 
-clusters    = [[2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [1, 15, 16, 17], [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]]
-init_fspace = [(8, 3), (4, 4), (3, 8)]
+clusters    = [[1,], [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14], [15, 16, 17], [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30]]
+init_fspace = [(1,1), (8, 3), (3, 3), (3, 8)]
 
 
 clusters = [MOCluster(i, collect(clusters[i])) for i = 1:length(clusters)]
 display(clusters)
 
-d1 = RDM1(n_orb(ints))
+d1 = RDM1(Pa,Pb)
 
 ansatze=[FCIAnsatz(length(ci),init_fspace[ci.idx][1],init_fspace[ci.idx][2]) for ci in clusters]
 
-# # Do CMF
-#e_cmf, U, d1 = ClusterMeanField.cmf_oo_newton(ints, clusters, init_fspace, ansatze, rdm1, maxiter_oo = 400,
-#                                              tol_oo=1e-6, 
-#                                              tol_d1=1e-9, 
-#                                              tol_ci=1e-11,
-#                                              verbose=4, 
-#                                              zero_intra_rots = true,
-#                                              sequential=true)
-e_cmf, U, d1 = ClusterMeanField.cmf_oo(ints, clusters, init_fspace, RDM1(n_orb(ints)),
-                                       verbose=2, sequential=true, max_iter_oo=100)
+# Do CMF
+e_cmf, U, d1 = ClusterMeanField.cmf_oo_newton(ints, clusters, init_fspace, ansatze, d1, maxiter_oo = 400,
+                                              tol_oo=1e-6, 
+                                              tol_d1=1e-9, 
+                                              tol_ci=1e-11,
+                                              verbose=4, 
+                                              zero_intra_rots = true,
+                                              sequential=true)
+#e_cmf, U, d1 = ClusterMeanField.cmf_oo(ints, clusters, init_fspace, RDM1(n_orb(ints)),
+#                                       verbose=2, sequential=true, max_iter_oo=100)
 
 ints = orbital_rotation(ints, U)
 C = C*U
